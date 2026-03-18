d0a6831726fbae3e35c03fd608cb56971bdf9c00
ErickYuXiangLi <lierick08@gmail.com>
2026-03-18 20:47:25 -0300

    aula 2 variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,44 @@
+print('hello mundo')
+
+print(7+4)
+print("7 +4")
+print("7 "+"4")
+
+'''comentrarios'''
+
+#variaveis
+nome = 'alexandre' #string
+print(nome)
+idade= 26 #int
+peso =70.2 #float
+
+print(nome,idade,peso)
+print(f"Oiii {nome}!!!")
+#input-simulação de um forms no cmd
+
+nome = input("digite seu nome:")
+idade =int(input("digite sua idade:"))
+peso =float(input("digite seu peso:"))
+print(nome,idade,peso)
+print(idade + 1)
+ano_nascimento =1976
+ano_atual = 2026
+print(ano_atual-ano_nascimento)
+
+
+#desafio 01
+nome = input("digite seu nome:")
+print(f" {nome} +boas-vindas ao fiap")
+
+#desafio 02
+dia = int(input("qual sua data de nascimento:"))
+mes = int(input("qual sua mes de nascimento:"))
+ano = int(input("qual sua ano de nascimento:"))
+
+print(f" {dia} /{mes} /{ano}")
+
+#desafio 03
+numero01=int(input("digite seu primeiro numero:"))
+numero02=int(input("digite seu segundo numero:"))
+
+print( numero01 + numero02)
